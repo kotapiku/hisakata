a7e49c8768ca534b77e6eac70c7352217569bb5d
kotapiku <ice08235523@gmail.com>
2017-06-23 16:59:11 +0900

    no message

--- mysite/settings.py
+++ mysite/settings.py
@@ -115,16 +115,19 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
-
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
 LOGIN_URL = '/hisakata/login/'
 LOGIN_REDIRECT_URL = '/hisakata/'
+
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 import dj_database_url
 DATABASES['default'] = dj_database_url.config()
@@ -132,8 +135,6 @@ DATABASES['default'] = dj_database_url.config()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 ALLOWED_HOSTS = ['*']
-
-STATIC_ROOT = 'static'
 
 DEBUG = False
 
