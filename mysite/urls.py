6fa9a57628e942aab640244afd4a6f8b86345844
kotapiku <ice08235523@gmail.com>
2017-07-04 13:37:23 +0900

    no message

--- mysite/urls.py
+++ mysite/urls.py
@@ -23,5 +23,10 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += url('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
+
+#
+# if settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
