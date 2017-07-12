e700fa1177d0430770b8738710188a5359b618e9
kotapiku <ice08235523@gmail.com>
2017-07-12 16:09:26 +0900

    修正

--- mysite/settings.py
+++ mysite/settings.py
@@ -48,12 +48,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
-
-MIDDLEWARE_CLASSES = (
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-)
-
 
 ROOT_URLCONF = 'mysite.urls'
 
